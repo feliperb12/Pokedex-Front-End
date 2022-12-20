0dd2aec4c900c8f335f87e01f8c594039ea46c81
FelipinDaMassa <feliperibeiroaraujo3@hotmail.com>
2022-12-20 09:29:43 -0300

    manipulando multpicas requisições em paralelo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,30 @@
-console.log("Sucesso!")
+function convertPokemonTypesToLi(pokemonTypes){
+  return pokemonTypes.map((typeSlot)=> `<li class="type">${typeSlot.type.name}</li> `)
+}
+
+function convertPokemonToHtml(pokemon) {
+  return `
+    <li class="pokemon">
+    <span class="number">#${pokemon.order}</span>
+    <span class="name">${pokemon.name}</span>
+    <div class="detail">
+        <ol class="types">
+            ${convertPokemonTypesToLi(pokemon.types).join('')}
+        </ol>
+        <img src="${pokemon.sprites.other.dream_world.front_default}"
+            alt=${pokemon.name}>
+
+    </div>
+</li>
+    
+    
+    `;
+}
+
+const pokemonList = document.getElementById("pokemonList");
+
+// ISSO É O PROMISE(PROMESA), COMO SE FOSSSE UM TRY, CATCH, FINALLY
+pokeApi.getPokemons().then((pokemons = []) => {
+  const newHtml = pokemons.map(convertPokemonToHtml).join("");
+  pokemonList.innerHTML = newHtml;
+});
